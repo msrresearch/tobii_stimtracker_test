051a2eca04d751ea3c478c8c3140081624f1d2b6
Björn Guth <bjoern.guth@rwth-aachen.de>
2018-01-22 15:27:49 +0100

    Add Stimtracker support to the test

--- tobii_stimtracker_test.py
+++ tobii_stimtracker_test.py
@@ -1,21 +1,26 @@
 from pygaze import libscreen, eyetracker
 import time
 import argparse
+import tobii_research as tr
 
 parser = argparse.ArgumentParser(description='A small test for pygaze using tobii eyetracker')
 parser.add_argument('-c', '--no_calibrate', default=False, action='store_true', help='skip calibration')
 args = parser.parse_args()
 
+def stimtracker_signal_callback(data):
+    print(data)
+
 display = libscreen.Display()
 screen = libscreen.Screen()
 eyetracker = eyetracker.EyeTracker(display)
+eyetracker.eyetracker.subscribe_to(tr.EYETRACKER_EXTERNAL_SIGNAL, stimtracker_signal_callback, as_dictionary=True)
 
 if not args.no_calibrate:
     eyetracker.calibrate()
 
 eyetracker.start_recording()
 
-black = False
+black = True
 
 for i in range (10, 0, -1):
     if black:
@@ -30,5 +35,6 @@ for i in range (10, 0, -1):
     time.sleep(1)
 
 eyetracker.stop_recording()
+eyetracker.eyetracker.unsubscribe_from(tr.EYETRACKER_EXTERNAL_SIGNAL, stimtracker_signal_callback)
 
 display.close()
